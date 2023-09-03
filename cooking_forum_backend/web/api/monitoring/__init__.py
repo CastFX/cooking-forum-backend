9c49b8d31ceac3d4f39d247ec4fb57747d2ff104
Christian Serra <castachris@hotmail.it>
2023-09-04 01:24:54 +0200

    feat: added monitoring routes

---
+++ cooking_forum_backend/web/api/monitoring/__init__.py
@@ -0,0 +1,4 @@
+"""API for checking project status."""
+from cooking_forum_backend.web.api.monitoring.views import router
+
+__all__ = ["router"]
--- cooking_forum_backend/web/api/router.py
+++ cooking_forum_backend/web/api/router.py
@@ -1,7 +1,8 @@
 from fastapi.routing import APIRouter
 
-from cooking_forum_backend.web.api import docs, auth
+from cooking_forum_backend.web.api import docs, auth, monitoring
 
 api_router = APIRouter()
 api_router.include_router(docs.router)
 api_router.include_router(auth.router)
+api_router.include_router(monitoring.router)
